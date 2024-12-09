410e16346c908eceb7af7d79b95309d3fb7f6f7b
kevduc <64347790+kevduc@users.noreply.github.com>
2024-12-09 00:51:05 +0000

    Loading while undefined

--- src/services/users.ts
+++ src/services/users.ts
@@ -6,10 +6,9 @@ type UsersDto = { users: User[] };
 export async function getUsers() {
   await pause(5000); // simulate long loading time
 
-  const data = { users: [] };
-  // (await fetch("https://dummyjson.com/users").then((res) =>
-  //   res.json(),
-  // )) as UsersDto;
+  const data = (await fetch("https://dummyjson.com/users").then((res) =>
+    res.json(),
+  )) as UsersDto;
 
   return data.users;
 }
