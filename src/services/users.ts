05de5685e65a8705287922710621e2447151330e
kevduc <64347790+kevduc@users.noreply.github.com>
2024-12-09 00:46:08 +0000

    Long loading time

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,8 +1,11 @@
+import { pause } from "@/utils/pause";
 import type { User } from "./user.types";
 
 type UsersDto = { users: User[] };
 
 export async function getUsers() {
+  await pause(5000); // simulate long loading time
+
   const data = (await fetch("https://dummyjson.com/users").then((res) =>
     res.json(),
   )) as UsersDto;
