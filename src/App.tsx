5fb16ed1f744f6549d9c718ad3e8c14f30e555f5
kevduc <64347790+kevduc@users.noreply.github.com>
2024-12-09 01:04:51 +0000

    Separate loading state

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,35 @@ import { useEffect, useState } from "react";
 import { getUsers } from "@/services/users";
 import type { User } from "@/services/user.types";
 import { Spinner } from "@/components/ui/Spinner";
+import { Alert } from "@/components/ui/Alert";
 
 export function App() {
-  const [users, setUsers] = useState<User[] | undefined>(undefined);
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<unknown>(undefined);
 
   useEffect(() => {
     async function fetchUsers() {
+      setIsLoading(true);
+
       const users = await getUsers();
       setUsers(users);
+      setIsLoading(false);
     }
 
-    fetchUsers();
+    fetchUsers().catch((error: unknown) => {
+      setIsLoading(false);
+      setError(error);
+    });
   }, []);
 
   return (
     <Container>
       <Title>Users</Title>
-      {users === undefined ? (
+      {isLoading ? (
         <Spinner />
+      ) : error !== undefined ? (
+        <Alert error={error} />
       ) : (
         <Flex>
           {users.map((user) => (
